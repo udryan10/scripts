30608982100c57d98055236bb60ab91d0db1e700
Ryan Grothouse <ryan.grothouse@ge.com>
2014-05-22 23:07:19 -0400

    initial commit

---
+++ batch-ssh.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/ruby
+# usage: 
+#      cat hosts.txt | xargs -n1 -P4 ./batch-ssh.rb # run 4 processes
+#      echo "foo.com" | xargs -n1 ./batch-ssh.rb
+
+require 'net/ssh'
+
+user = 'user'
+password = 'password'
+
+command = "ls"
+ARGV.each do |a|
+  begin
+    Net::SSH.start( a, user, :password => password ) do |ssh|
+      result = ssh.exec!(command)
+      puts "=" * 20 + a + "=" * 20
+      puts result
+      puts "=" * 20 + a + "=" * 20
+    end
+  rescue => e
+    puts "Caught exception: #{e.message}"
+  end
+end
