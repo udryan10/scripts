a13a11cbf256b0fe76b5b175eb1bcc57149e49bf
Ryan Grothouse <ryan.grothouse@ge.com>
2014-05-23 14:11:37 -0400

    use open_channel so we can pass require_pty

--- batch-ssh.rb
+++ batch-ssh.rb
@@ -2,21 +2,25 @@
 # usage: 
 #      cat hosts.txt | xargs -n1 -P4 ./batch-ssh.rb # run 4 processes
 #      echo "foo.com" | xargs -n1 ./batch-ssh.rb
-
 require 'net/ssh'
 
 user = 'user'
 password = 'password'
 
-command = "ls"
+command = "hostname"
 
 ARGV.each do |a|
   begin
     Net::SSH.start( a, user, :password => password ) do |ssh|
-      result = ssh.exec!(command)
-      puts "=" * 20 + a + "=" * 20
-      puts result
-      puts "=" * 20 + a + "=" * 20
+      ssh.open_channel do |channel|
+        channel.on_data do |ch, data|
+          puts "=" * 20 + a + "=" * 20
+          puts data
+          puts "=" * 20 + a + "=" * 20
+        end
+        channel.request_pty
+        channel.exec command
+      end
     end
   rescue => e
     puts "Caught exception: #{e.message}"
